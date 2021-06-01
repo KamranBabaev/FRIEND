2957d0572a122a8195c8c476bbbf4a85b85fca9e
Kamran Babaev <babaevkamran2094@gmail.com>
2021-06-01 08:08:21 +0300

    2 pause

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,8 +4,8 @@ import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from 'react-router-dom';
-import { StateType } from './redux/state';
+import {Route} from 'react-router-dom';
+import {StateType} from './redux/state';
 
 type AppPropsType = {
     state: StateType
@@ -13,24 +13,22 @@ type AppPropsType = {
 
 function App(props: AppPropsType) {
     return (
-        <BrowserRouter>
-            <div className="App-wrapper">
-                <Header/>
-                <Navbar friends={props.state.sidebar.friend}/>
-                <div className='App-wrapper-content'>
+        <div className="App-wrapper">
+            <Header/>
+            <Navbar friends={props.state.sidebar.friend}/>
+            <div className='App-wrapper-content'>
 
-                    <Route path='/profile'
-                           render={() => <Profile
-                               posts={props.state.profilePage.posts}/>}/>
+                <Route path='/profile'
+                       render={() => <Profile
+                           posts={props.state.profilePage.posts}/>}/>
 
-                    <Route exact path='/dialogs'
-                           render={() => <Dialogs
-                               dialogs={props.state.messagePage.dialogs}
-                               messages={props.state.messagePage.messages}/>}/>
+                <Route exact path='/dialogs'
+                       render={() => <Dialogs
+                           dialogs={props.state.messagePage.dialogs}
+                           messages={props.state.messagePage.messages}/>}/>
 
-                </div>
             </div>
-        </BrowserRouter>
+        </div>
     )
 }
 
