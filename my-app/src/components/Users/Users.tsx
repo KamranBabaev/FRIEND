c6cc8bcf6476bf12f8b5c60763d5db33f0865fac
Kamran Babaev <babaevkamran2094@gmail.com>
2021-06-29 17:05:19 +0300

    54 part finish

--- my-app/src/components/Users/Users.tsx
+++ my-app/src/components/Users/Users.tsx
@@ -13,8 +13,7 @@ type UsersPropsType = {
 
 export class Users extends React.Component<UsersPropsType> {
 
-    constructor(props: UsersPropsType) {
-        super(props);
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(resp => this.props.setUsers(resp.data.items))
     }
