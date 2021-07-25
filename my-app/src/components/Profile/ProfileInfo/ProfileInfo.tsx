98b1ec86bb5b1c0cb0110007ef27b026f65b45a6
Kamran Babaev <babaevkamran2094@gmail.com>
2021-07-25 08:06:05 +0300

    refactoring

--- my-app/src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ my-app/src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -5,41 +5,47 @@ import {Preloader} from "../../common/Preloader/Preolader";
 import {ProfileDataType} from "../ProfileContainer";
 
 type ProfileInfoPropsType = {
-    profile: ProfileDataType
+  profile: ProfileDataType
 }
 
 export const ProfileInfo = (props: ProfileInfoPropsType) => {
 
-    if (!props.profile) {
-        return <Preloader/>
-    }
+  if (!props.profile) {
+    return <Preloader/>
+  }
 
-    return (
-        <div className={style.profileInfo}>
-            <div className={style.user}>
-                <img src={avatar}/>
-                <div className={style.inform}>
-                    <div className={style.name}>КАМРАН БАБАЕВ</div>
-                    возраст: 27 лет<br/>статус: студент<br/>город: Санкт-Петербург, Россия
-                </div>
-            </div>
-
-            <div className={style.user}>
-                <div className={style.inform}>
-                    <img src={props.profile.photos.small} alt=''/>
-                    <div className={style.name}>{props.profile.fullName}</div>
-                    <div className={style.aboutMe}>Обо мне: {props.profile.aboutMe}</div>
-                    <div className={style.jobStatus}>
-                        {
-                            props.profile.lookingForAJob
-                                ? <div>Статус: работаю</div>
-                                : <div>Статус: в поисках работы</div>
-                        }
-                    </div>
-                    <div className={style.contacts}>Мой инстаграмм: {props.profile.contacts.instagram}</div>
-                </div>
-            </div>
-
+  return (
+      <div className={style.profileInfo}>
+        <div className={style.user}>
+          <img src={avatar}/>
+          <div className={style.inform}>
+            <div className={style.name}>КАМРАН БАБАЕВ</div>
+            возраст: 27 лет<br/>
+            статус: студент<br/>
+            город: Санкт-Петербург, Россия
+          </div>
         </div>
-    )
+
+        <div className={style.user}>
+          <div className={style.inform}>
+            <img src={props.profile.photos.small} alt=''/>
+            <div className={style.name}>{props.profile.fullName}</div>
+            <div className={style.aboutMe}>
+              Обо мне: {props.profile.aboutMe}
+            </div>
+            <div className={style.jobStatus}>
+              {
+                props.profile.lookingForAJob
+                    ? <div>Статус: работаю</div>
+                    : <div>Статус: в поисках работы</div>
+              }
+            </div>
+            <div className={style.contacts}>
+              Мой инстаграмм: {props.profile.contacts.instagram}
+            </div>
+          </div>
+        </div>
+
+      </div>
+  )
 }
