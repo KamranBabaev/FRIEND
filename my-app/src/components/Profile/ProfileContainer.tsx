353985f2e6e494472a51ba7619985edbf197179e
Kamran Babaev <babaevkamran2094@gmail.com>
2021-07-25 09:37:25 +0300

    71 part, add status for users

--- my-app/src/components/Profile/ProfileContainer.tsx
+++ my-app/src/components/Profile/ProfileContainer.tsx
@@ -11,61 +11,64 @@ import {ProfilePageType} from "../../redux/store";
 
 
 export type ProfileDataType = {
-    aboutMe: string
-    contacts: {
-        facebook: string
-        website: string | null
-        vk: string
-        twitter: string
-        instagram: string
-        youtube: string | null
-        github: string
-        mainLink: string | null
-    }
-    lookingForAJob: boolean
-    lookingForAJobDescription: string
-    fullName: string
-    userId: string
-    photos: {
-        small: string
-        large: string
-    }
+  aboutMe: string
+  contacts: {
+    facebook: string
+    website: string | null
+    vk: string
+    twitter: string
+    instagram: string
+    youtube: string | null
+    github: string
+    mainLink: string | null
+  }
+  lookingForAJob: boolean
+  lookingForAJobDescription: string
+  fullName: string
+  userId: string
+  photos: {
+    small: string
+    large: string
+  }
 }
 
 type MapStateToPropsType = {
-    profile: ProfilePageType
+  profile: ProfilePageType
 }
 
 type MapDispatchToPropsType = {
-    getUsersProfile: (userId: number) => void
+  getUsersProfile: (userId: number) => void
 }
 
-type ProfileContainerPropsType = RouteComponentProps<{ userId?: string }> & {
-    profile: ProfileDataType
-    getUsersProfile: (userId: string) => void
+type ProfileContainerPropsType = RouteComponentProps<{ userId?: string }>
+    & {
+  profile: ProfileDataType
+  getUsersProfile: (userId: string) => void
 }
 
-class ProfileContainer extends React.Component<ProfileContainerPropsType & MapStateToPropsType & MapDispatchToPropsType> {
+class ProfileContainer extends React.Component<ProfileContainerPropsType
+    & MapStateToPropsType
+    & MapDispatchToPropsType> {
 
-    componentDidMount() {
-        let userId = this.props.match.params.userId
-        if (!userId) {
-            userId = '2'
-        }
-        this.props.getUsersProfile(userId)
+  componentDidMount() {
+    let userId = this.props.match.params.userId
+    if (!userId) {
+      userId = '2'
     }
+    this.props.getUsersProfile(userId)
+  }
 
-    render() {
-        return (
-            <div className={style.profile}>
-                <Profile {...this.props} profile={this.props.profile}/>
-            </div>
-        )
-    }
+  render() {
+    return (
+        <div className={style.profile}>
+          <Profile {...this.props} profile={this.props.profile}/>
+        </div>
+    )
+  }
 }
 
 let mapStateToProps = (state: any) => ({
-    profile: state.profilePage.profile,
+  profile: state.profilePage.profile,
 })
 
 export default compose<ComponentType>(
--- my-app/src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ my-app/src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -3,6 +3,7 @@ import style from './ProfileInfo.module.css'
 import avatar from './avatar.jpg'
 import {Preloader} from "../../common/Preloader/Preolader";
 import {ProfileDataType} from "../ProfileContainer";
+import {ProfileStatus} from "../ProfileStatus/ProfileStatus";
 
 type ProfileInfoPropsType = {
   profile: ProfileDataType
@@ -14,31 +15,36 @@ export const ProfileInfo = (props: ProfileInfoPropsType) => {
     return <Preloader/>
   }
 
+  const statusWork = () => {
+    return (
+        props.profile.lookingForAJob
+            ? <div>Статус: работаю</div>
+            : <div>Статус: в поисках работы</div>
+    )
+  }
+
   return (
       <div className={style.profileInfo}>
         <div className={style.user}>
           <img src={avatar}/>
           <div className={style.inform}>
             <div className={style.name}>КАМРАН БАБАЕВ</div>
-            возраст: 27 лет<br/>
-            статус: студент<br/>
-            город: Санкт-Петербург, Россия
+            <ProfileStatus status='изменение статуса'/>
+            <div>возраст: 27 лет</div>
+            <div>занятость: студент</div>
+            <div>город: Санкт-Петербург, Россия</div>
           </div>
         </div>
-
         <div className={style.user}>
           <div className={style.inform}>
             <img src={props.profile.photos.small} alt=''/>
             <div className={style.name}>{props.profile.fullName}</div>
-            <div className={style.aboutMe}>
-              Обо мне: {props.profile.aboutMe}
+            <ProfileStatus status='изменение статуса'/>
+            <div className={style.aboutMe}>Обо мне:
+              {props.profile.aboutMe}
             </div>
             <div className={style.jobStatus}>
-              {
-                props.profile.lookingForAJob
-                    ? <div>Статус: работаю</div>
-                    : <div>Статус: в поисках работы</div>
-              }
+              {statusWork()}
             </div>
             <div className={style.contacts}>
               Мой инстаграмм: {props.profile.contacts.instagram}
